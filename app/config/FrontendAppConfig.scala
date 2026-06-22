86636a1705753e0ab911a4703997d34f22d4e586
cookEbox <67757166+cookEbox@users.noreply.github.com>
2026-06-22 13:43:22 +0100

    removed mongo dependencies

--- app/config/FrontendAppConfig.scala
+++ app/config/FrontendAppConfig.scala
@@ -50,7 +50,4 @@ class FrontendAppConfig @Inject() (configuration: Configuration) {
 
   val timeout: Int   = configuration.get[Int]("timeout-dialog.timeout")
   val countdown: Int = configuration.get[Int]("timeout-dialog.countdown")
-
-  val cacheTtl: Long = configuration.get[Int]("mongodb.timeToLiveInSeconds")
-
 }
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,17 +7,15 @@ object AppDependencies {
 
   val compile = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"    % "12.8.0",
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"    % bootstrapVersion,
-    "uk.gov.hmrc"       %% "catalogue-wrapper-play-30"     % "0.1.0-SNAPSHOT",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"            % hmrcMongoVersion
-
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30" % "12.8.0",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30" % bootstrapVersion,
+    "uk.gov.hmrc"       %% "catalogue-wrapper-play-30"  % "0.1.0-SNAPSHOT"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-30"  % bootstrapVersion,
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
-    "org.scalatestplus"       %% "scalacheck-1-17"         % "3.2.17.0"
+    "uk.gov.hmrc"       %% "bootstrap-test-play-30"     % bootstrapVersion,
+    "org.scalatestplus" %% "scalacheck-1-17"            % "3.2.17.0",
+    "uk.gov.hmrc"       %% "catalogue-wrapper-play-30"  % "0.1.0-SNAPSHOT"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
